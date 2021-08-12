abc3c43069985aa4fd4ba938f2b9b243e01efea2
Tomas Valenta <semanticer@gmail.com>
2021-08-12 14:49:22 +0200

    Add state testing

---
+++ komposable-architecture/src/test/java/com/toggl/komposable/StoreStateTests.kt
@@ -0,0 +1,92 @@
+package com.toggl.komposable
+
+import app.cash.turbine.test
+import com.toggl.komposable.common.StoreCoroutineTest
+import com.toggl.komposable.common.TestAction
+import com.toggl.komposable.common.TestEffect
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.Test
+import kotlin.time.ExperimentalTime
+
+class StoreStateTests : StoreCoroutineTest() {
+
+    @ExperimentalTime
+    @Test
+    fun `state is emitted in batches after all dispatched actions were reduced`() = runBlockingTest {
+        testStore.state.test {
+            testStore.dispatch(
+                listOf(
+                    TestAction.DoNothingAction,
+                    TestAction.ChangeTestProperty("123"),
+                    TestAction.AddToTestProperty("4"),
+                    TestAction.DoNothingAction
+                )
+            )
+
+            // initial state
+            awaitItem().testProperty shouldBe ""
+            // TestAction.DoNothingAction
+            // TestAction.ChangeTestProperty("123")
+            // TestAction.AddToTestProperty("4")
+            // TestAction.DoNothingAction
+            awaitItem().testProperty shouldBe "1234"
+
+            cancelAndConsumeRemainingEvents()
+        }
+    }
+
+    @ExperimentalTime
+    @Test
+    fun `new state is emitted only when it's different than the previous one`() = runBlockingTest {
+        testStore.state.test {
+            testStore.dispatch(
+                listOf(
+                    TestAction.DoNothingAction,
+                    TestAction.ChangeTestProperty("123"),
+                    TestAction.AddToTestProperty("4"),
+                    TestAction.ChangeTestProperty("")
+                )
+            )
+
+            // initial state
+            awaitItem().testProperty shouldBe ""
+            // TestAction.DoNothingAction
+            // TestAction.ChangeTestProperty("123")
+            // TestAction.ChangeTestProperty("")
+            // we got back to the initial state, it doesn't make sense to emit anything
+
+            cancelAndConsumeRemainingEvents()
+        }
+    }
+
+    @ExperimentalTime
+    @Test
+    fun `effects emit new state only after the result state of previously dispatched actions was emitted`() = runBlockingTest {
+        testStore.state.test {
+            testStore.dispatch(
+                listOf(
+                    TestAction.DoNothingAction,
+                    TestAction.ChangeTestProperty("123"),
+                    TestAction.AddToTestProperty("4"),
+                    TestAction.StartEffectAction(TestEffect(TestAction.DoNothingFromEffectAction)),
+                    TestAction.StartEffectAction(TestEffect(TestAction.ClearTestPropertyFromEffect))
+                )
+            )
+
+            // initial state
+            awaitItem().testProperty shouldBe ""
+            // TestAction.DoNothingAction
+            // TestAction.ChangeTestProperty("123")
+            // TestAction.AddToTestProperty("4")
+            // TestAction.StartEffectAction(TestEffect(TestAction.DoNothingFromEffectAction))
+            // TestAction.StartEffectAction(TestEffect(TestAction.ClearTestPropertyFromEffect))
+            awaitItem().testProperty shouldBe "1234"
+            // From effect.execute() => TestAction.DoNothingFromEffectAction
+            // From effect.execute() => TestAction.ClearTestPropertyFromEffect
+            awaitItem().testProperty shouldBe ""
+
+            cancelAndConsumeRemainingEvents()
+        }
+    }
+}
