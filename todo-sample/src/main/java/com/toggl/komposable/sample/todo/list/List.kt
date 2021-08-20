75ce39a18c37a3b03d9f4a5944b3fbfe79c9fde7
Tomas Valenta <semanticer@gmail.com>
2021-08-20 20:26:08 +0200

    Add empty list state

--- todo-sample/src/main/java/com/toggl/komposable/sample/todo/list/List.kt
+++ todo-sample/src/main/java/com/toggl/komposable/sample/todo/list/List.kt
@@ -3,6 +3,8 @@ package com.toggl.komposable.sample.todo.list
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.FloatingActionButton
@@ -12,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.toggl.komposable.sample.todo.collectAsStateWhenStarted
@@ -29,13 +32,21 @@ fun ListPage() {
 
 @Composable
 private fun TodoList(todoList: List<TodoItem>) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        contentPadding = PaddingValues(16.dp)
-    ) {
-        items(
-            items = todoList,
-            itemContent = { TodoItem(it) }
+    if (todoList.isNotEmpty()) {
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            items(
+                items = todoList,
+                itemContent = { TodoItem(it) }
+            )
+        }
+    } else {
+        Text(
+            modifier = Modifier.fillMaxSize().padding(48.dp),
+            text = "Add your first Todo!",
+            style = MaterialTheme.typography.h3
         )
     }
 }
