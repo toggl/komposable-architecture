ff56da54bc8da32f16b14f5a354690e37e3bf358
Tomas Valenta <semanticer@gmail.com>
2021-09-13 16:11:13 +0200

    Add sample ListReducer tests

---
+++ todo-sample/src/test/java/com/toggl/komposable/sample/todo/TestListReducer.kt
@@ -0,0 +1,41 @@
+package com.toggl.komposable.sample.todo
+
+import com.toggl.komposable.extensions.noEffect
+import com.toggl.komposable.sample.todo.common.CoroutineTest
+import com.toggl.komposable.sample.todo.common.testReduce
+import com.toggl.komposable.sample.todo.data.TodoItem
+import com.toggl.komposable.sample.todo.list.ListAction
+import com.toggl.komposable.sample.todo.list.ListReducer
+import com.toggl.komposable.sample.todo.list.ListState
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class TestListReducer : CoroutineTest() {
+    private val reducer = ListReducer()
+    private val testTodoItem = TodoItem(1, "title", "description")
+
+    @Test
+    fun `ListUpdated action should update the list of todos and return no effects`() = runBlockingTest {
+        val initialState = ListState(todoList = emptyList(), backStack = emptyList())
+        reducer.testReduce(
+            initialState,
+            ListAction.ListUpdated(listOf(testTodoItem))
+        ) { state, effects ->
+            assertEquals(initialState.copy(todoList = listOf(testTodoItem)), state)
+            assertEquals(noEffect(), effects)
+        }
+    }
+
+    @Test
+    fun `AddTodoTapped action should add new destination to the backstack and return no effects`() = runBlockingTest {
+        val initialState = ListState(todoList = emptyList(), backStack = emptyList())
+        reducer.testReduce(
+            initialState,
+            ListAction.AddTodoTapped
+        ) { state, effects ->
+            assertEquals(initialState.copy(backStack = listOf(AppDestination.Add)), state)
+            assertEquals(noEffect(), effects)
+        }
+    }
+}
