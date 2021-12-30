af9750f7d7cc9d746895308ece922e07af1a7862
Tomas Valenta <semanticer@gmail.com>
2021-12-30 15:37:59 +0100

    📦 Update Kotlin, Compose and other dependencies

--- buildSrc/src/main/java/com/toggl/komposable/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/toggl/komposable/buildsrc/dependencies.kt
@@ -5,7 +5,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.2"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.4"
     const val gradleVersionsPlugin = "com.github.ben-manes:gradle-versions-plugin:0.36.0"
     const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.7"
     // fixes warning thrown by SLF4J
@@ -33,7 +33,7 @@ object Libs {
     }
 
     object Kotlin {
-        const val version = "1.5.21"
+        const val version = "1.6.0"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
 
@@ -45,7 +45,7 @@ object Libs {
     }
 
     object Coroutines {
-        private const val version = "1.5.2"
+        private const val version = "1.6.0"
         const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
         const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
 
@@ -57,16 +57,16 @@ object Libs {
     object AndroidX {
 
         object Activity {
-            const val version = "1.3.0"
+            const val version = "1.4.0"
             const val activityKtx = "androidx.activity:activity-ktx:$version"
             const val activityCompose = "androidx.activity:activity-compose:$version"
         }
 
-        const val appcompat = "androidx.appcompat:appcompat:1.3.0"
+        const val appcompat = "androidx.appcompat:appcompat:1.4.0"
         const val coreKtx = "androidx.core:core-ktx:1.6.0"
 
         object Compose {
-            const val version = "1.0.1"
+            const val version = "1.1.0-rc01"
             const val runtime = "androidx.compose.runtime:runtime:$version"
             const val compiler = "androidx.compose.compiler:compiler:$version"
             const val animation = "androidx.compose.animation:animation:$version"
@@ -102,7 +102,7 @@ object Libs {
         }
 
         object Lifecycle {
-            private const val version = "2.4.0-beta01"
+            private const val version = "2.4.0"
             const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
             const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07"
             const val ktx = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
@@ -134,7 +134,7 @@ object Libs {
     }
 
     object Hilt {
-        private const val version = "2.38.1"
+        private const val version = "2.40.5"
 
         const val gradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
         const val hilt = "com.google.dagger:hilt-android:$version"
