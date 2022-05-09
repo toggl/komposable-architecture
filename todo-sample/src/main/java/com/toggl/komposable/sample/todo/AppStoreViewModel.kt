7b823940025933cb8e0dbaa0fe9d230071043169
Tomas Valenta <semanticer@gmail.com>
2022-05-09 16:11:50 +0200

    App state transition

--- todo-sample/src/main/java/com/toggl/komposable/sample/todo/AppStoreViewModel.kt
+++ todo-sample/src/main/java/com/toggl/komposable/sample/todo/AppStoreViewModel.kt
@@ -2,10 +2,21 @@ package com.toggl.komposable.sample.todo
 
 import androidx.lifecycle.ViewModel
 import com.toggl.komposable.architecture.Store
+import com.toggl.komposable.sample.todo.data.Identity
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class AppStoreViewModel @Inject constructor(
     store: Store<AppState, AppAction>
-) : ViewModel(), Store<AppState, AppAction> by store
+) : ViewModel(), ViewStateProvider<AppViewState>, Store<AppState, AppAction> by store {
+    override val viewState: Flow<AppViewState> = state.map { AppViewState(it.identity, it.backStack) }
+    override val initialViewState: AppViewState = AppViewState()
+}
+
+data class AppViewState(
+    val identity: Identity = Identity.Unknown,
+    val backStack: BackStack = listOf(AppDestination.List),
+)
