a35a16e8c295f4a78cdb1b15d058535504492499
William Barbosa <heytherewill@gmail.com>
2022-08-29 16:59:24 +0200

    📝 Fix documentation copy (#34)

--- komposable-architecture/src/main/java/com/toggl/komposable/extensions/SubscriptionExtensions.kt
+++ komposable-architecture/src/main/java/com/toggl/komposable/extensions/SubscriptionExtensions.kt
@@ -4,25 +4,25 @@ import com.toggl.komposable.architecture.Subscription
 import com.toggl.komposable.internal.CompositeSubscription
 
 /**
- * @param subscriptions      List of subscriptions which should be merged
- * @return A [CompositeSubscription] of a given subscriptions
+ * @param subscriptions      List of subscriptions which should be merged.
+ * @return A [CompositeSubscription] of a given subscriptions.
  * @see CompositeSubscription
  */
 fun <State, Action : Any> mergeSubscriptions(vararg subscriptions: Subscription<State, Action>): Subscription<State, Action> =
     CompositeSubscription(subscriptions.toList())
 
 /**
- * @param subscriptions      List of subscriptions which should be merged
- * @return A [CompositeSubscription] of a given subscriptions
+ * @param subscriptions      List of subscriptions which should be merged.
+ * @return A [CompositeSubscription] of a given subscriptions.
  * @see CompositeSubscription
  */
 fun <State, Action : Any> mergeSubscriptions(subscriptions: List<Subscription<State, Action>>): Subscription<State, Action> =
     CompositeSubscription(subscriptions)
 
 /**
- * @receiver First subscription
- * @param subscription      Second subscription that will be merged with the receiver
- * @return A [CompositeSubscription] of a given subscriptions
+ * @receiver First subscription.
+ * @param subscription      Second subscription that will be merged with the receiver.
+ * @return A [CompositeSubscription] of a given subscriptions.
  * @see CompositeSubscription
  */
 infix fun <State, Action : Any> Subscription<State, Action>.mergeWith(subscription: Subscription<State, Action>): Subscription<State, Action> =
