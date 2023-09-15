6108371757f9d82790c71a3a263ea22b16befeaa
Tomas Valenta <semanticer@gmail.com>
2023-09-15 13:36:03 +0200

    📦 Bump up library versions including AGP

--- todo-sample/build.gradle.kts
+++ todo-sample/build.gradle.kts
@@ -14,12 +14,12 @@ kapt {
 android {
     namespace = "com.toggl.komposable.sample.todo"
 
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.toggl.komposable.sample.todo"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- todo-sample/src/main/java/com/toggl/komposable/sample/todo/data/AppDatabase.kt
+++ todo-sample/src/main/java/com/toggl/komposable/sample/todo/data/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.toggl.komposable.sample.todo.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [TodoItem::class], version = 1)
+@Database(entities = [TodoItem::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun todoDao(): TodoDao
 }
