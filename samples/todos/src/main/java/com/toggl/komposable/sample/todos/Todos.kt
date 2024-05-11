1f5a57fda19e7edfb63e7038a65390643dac01c6
Daivid S. Silverio <doivid@hey.com>
2024-05-10 20:30:00 -0400

    🔧 Update sample

--- samples/todos/src/main/java/com/toggl/komposable/sample/todos/Todos.kt
+++ samples/todos/src/main/java/com/toggl/komposable/sample/todos/Todos.kt
@@ -8,9 +8,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.toggl.komposable.architecture.ReduceResult
 import com.toggl.komposable.architecture.Reducer
-import com.toggl.komposable.extensions.withSuspendEffect
+import com.toggl.komposable.extensions.debounce
+import com.toggl.komposable.extensions.withEffect
 import com.toggl.komposable.extensions.withoutEffect
-import kotlinx.coroutines.delay
 import java.util.UUID
 
 sealed class TodosAction {
@@ -52,10 +52,7 @@ class TodosReducer : Reducer<TodosState, TodosAction> {
             TodosAction.SortCompletedTodos ->
                 state.copy(todos = state.todos.sortedBy { it.isComplete }).withoutEffect()
             is TodosAction.Todo -> if (action.action is TodoAction.IsCompleteChanged) {
-                state.withSuspendEffect(id = "sort", cancelInFlight = true) {
-                    delay(1000)
-                    TodosAction.SortCompletedTodos
-                }
+                state.withEffect { of(TodosAction.SortCompletedTodos).debounce("sort", 1000) }
             } else {
                 state.withoutEffect()
             }
