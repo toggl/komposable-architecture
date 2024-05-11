0923cedd00ba452236d6f1529bbcf0acd1ea2e87
Daivid S. Silverio <doivid@hey.com>
2024-05-10 20:29:31 -0400

    🔧 Use what we already have

--- komposable-architecture/src/main/java/com/toggl/komposable/extensions/Debounce.kt
+++ komposable-architecture/src/main/java/com/toggl/komposable/extensions/Debounce.kt
@@ -1,28 +1,16 @@
 package com.toggl.komposable.extensions
 
 import com.toggl.komposable.architecture.Effect
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.sync.withLock
 
 fun <Action> Effect<Action>.debounce(
     id: Any,
     delayMillis: Long,
 ): Effect<Action> = Effect {
     this.run().debounce(delayMillis).onStart {
-        mutex.withLock {
-            cancellationJobs[id]?.forEach {
-                it.cancel(EffectCancellationException.InFlightEffectsCancelled(id))
-            }
-            cancellationJobs.remove(id)
-
-            val job = currentCoroutineContext()[Job]
-            job?.let { cancellationJobs.getOrPut(id) { mutableSetOf() }.add(it) }
-        }
         delay(delayMillis)
-    }.cancellable()
-}
+    }
+}.cancellable(id, true)
