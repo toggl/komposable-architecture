6156856bb2fc2bb495a17cb4f159405322856d4a
Tomas Valenta <semanticer@gmail.com>
2024-11-06 12:18:20 +0100

    Refactor and expand sample code to demonstrate the new code-generation capabilities

---
+++ samples/todos/src/main/java/com/toggl/komposable/sample/todos/title/TitleAction.kt
@@ -0,0 +1,7 @@
+package com.toggl.komposable.sample.todos.title
+
+sealed class TitleAction {
+    data class TitleChanged(val text: String) : TitleAction()
+    data object StartEditing : TitleAction()
+    data object StopEditing : TitleAction()
+}
